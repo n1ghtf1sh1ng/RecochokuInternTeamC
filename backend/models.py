8560c988c783c9f47201129be3db6c67c2eb65e5
Ry268 <ryotaro545@gmail.com>
2022-11-13 10:52:58 +0900

    add db model

--- backend/models.py
+++ backend/models.py
@@ -8,6 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, unique=True)
     genres = relationship("Genre", back_populates="owner")
 
 
@@ -15,7 +16,7 @@ class Genre(Base):
     __tablename__ = "genres"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String, index=True, unique=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="genres")
