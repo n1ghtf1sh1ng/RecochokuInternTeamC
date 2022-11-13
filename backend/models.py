8af3446f33af3d443cdbc5a9f597217e8dd426c1
Ry268 <ryotaro545@gmail.com>
2022-11-13 10:39:27 +0900

    add db model

--- backend/models.py
+++ backend/models.py
@@ -8,19 +8,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
+    genres = relationship("Genre", back_populates="owner")
 
 
-class Item(Base):
-    __tablename__ = "items"
+class Genre(Base):
+    __tablename__ = "genres"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="items")
+    owner = relationship("User", back_populates="genres")
