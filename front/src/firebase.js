d5103668ed5881b39cede9ea8827586c0c1eb0a2
nakashi94 <n0904yBUNJOU@gmail.com>
2022-11-13 10:52:59 +0900

    googleauth

---
+++ front/src/firebase.js
@@ -0,0 +1,19 @@
+// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvqKFv6daUIydLqf1ZdufziKDsvmJlYBY",
+  authDomain: "recochoku-5be7f.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,10 +1,43 @@
+import { signInWithPopup, User } from "firebase/auth";
 import { FC, memo } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+// import { useRecoilState } from "recoil";
+
+import { auth, provider } from "../firebase";
+// import { userState } from "../store/userState";
 
 export const Home: FC = memo(() => {
+  const [user] = useAuthState(auth);
+  // const [userInfo, setUserInfo] = useRecoilState<User | null | undefined>(
+  //   userState
+  // );
+  // setUserInfo(user);
+  // const setUser = useRecoilState(userState);
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("Googleアカウントでログインしました。");
+        console.log(result);
+        // console.log(userInfo);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>App Name</h1>
-      <div>ログイン</div>
+      {user ? (
+        <>
+          <img src={auth.currentUser?.photoURL ?? undefined} />
+          {/* <p>{userInfo?.email}</p> */}
+          <button onClick={() => auth.signOut()}>サインアウト</button>
+        </>
+      ) : (
+        <button onClick={signInWithGoogle}>Googleアカウントでログイン</button>
+      )}
     </div>
   );
 });
