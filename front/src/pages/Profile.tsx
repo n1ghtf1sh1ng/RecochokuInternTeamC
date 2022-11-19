4ccdf014a92d6229b236e6c40a25fe8826823dfb
nakashi94 <n0904yBUNJOU@gmail.com>
2022-11-19 12:11:07 +0900

    プロフィールのメールアドレス項目を削除

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -37,19 +37,6 @@ export const Profile: FC = memo(() => {
                 <div className="mb-6">
                   <div className="flex my-2">
                     <label className="text-xl font-bold mr-6 w-48">
-                      メールアドレス
-                    </label>
-                    <input
-                      type="text"
-                      className="p-2 pl-6 rounded w-full border-black border-2"
-                      readOnly={true}
-                      value={"n0904yBUNJOU@gmail.com"}
-                    />
-                  </div>
-                </div>
-                <div className="mb-6">
-                  <div className="flex my-2">
-                    <label className="text-xl font-bold mr-6 w-48">
                       カテゴリ
                     </label>
                     <select
