2025d0a66aaf030291346d490ecbe54c2d583ae6
nakashi94 <n0904yBUNJOU@gmail.com>
2022-11-20 23:25:53 +0900

    edit env variable

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -1,13 +1,9 @@
-// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvqKFv6daUIydLqf1ZdufziKDsvmJlYBY",
-  authDomain: "recochoku-5be7f.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
@@ -16,7 +12,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider().addScope('https://www.googleapis.com/auth/calendar.readonly');
+export const provider = new GoogleAuthProvider();
 
 // Google Calendar API を使用する場合はこのスコープを追加する。
 // export const provider = new GoogleAuthProvider().addScope("https://www.googleapis.com/auth/calendar.settings.readonly");
